8b2650b6b4c2f5deb58c5ed94efd457e170b9a15
sash-binary <sash@binary.com>
2020-04-15 13:50:08 +0300

    update

--- grunt/buildcontrol.js
+++ grunt/buildcontrol.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
         },
         pages: {
           options: {
-            remote: 'git@github.com:sash-binary/binarybot-landing.git',
+            remote: 'git@github.com:sash-binary/amp.git',
             branch: 'gh-pages'
           }
         },
